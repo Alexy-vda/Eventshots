3e2b82b1e9b2cd37817d32faeecae2659a54775d
Alexy <alexy.vda@outlook.fr>
2025-10-27 14:17:59 +0100

    feat: add Prisma Accelerate extension (optional)

--- lib/db.ts
+++ lib/db.ts
@@ -1,35 +1,12 @@
 import { PrismaClient } from "@prisma/client";
-import path from "path";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const getDatabaseUrl = () => {
-  const dbUrl = process.env.DATABASE_URL;
-  if (!dbUrl) {
-    throw new Error("DATABASE_URL is not defined");
-  }
-
-  if (dbUrl.startsWith("file:./")) {
-    const absolutePath = path.resolve(
-      process.cwd(),
-      dbUrl.replace("file:./", "")
-    );
-    return `file:${absolutePath}`;
-  }
-
-  return dbUrl;
-};
-
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    datasources: {
-      db: {
-        url: getDatabaseUrl(),
-      },
-    },
     log:
       process.env.NODE_ENV === "development"
         ? ["query", "error", "warn"]
